80643f1eff18a881594fcfeecfb6a75ac1a41295
hazemelwaleed45 <hazemelwaleed45@gmail.com>
2024-09-11 01:27:14 +0300

    impelment all CRUD from national holidays

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HolidayController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,8 +15,26 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/' , function(){ 
+    return 'Hello';}
+    ) ;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 // Route::post('users', [UserController::class, 'store']);
+
+Route::apiResource('holidays',HolidayController::class);
+
+// Route::get('holidays',[HolidayController::class,'index']);
+// Route::get('/holidays/{id}',[HolidayController::class,'show'])->name('holidays.view');
+
+// Route::delete('/holidays/{id}', [HolidayController::class, 'destroy'])->name('holidays.destroy');
+
+// Route::post('/holidays/store',[HolidayController::class,'store'])->name('holidays.store');
+
+// Route::get('/holidays/{id}/edit',[HolidayController::class,'edit'])->name('holidays.edit');
+
+// Route::put('/holidays/{id}/update',[HolidayController::class,'update'])->name('holidays.update');
+
+// Route::get('/holidays/{id}/create',[HolidayController::class,'create'])->name('holidays.create');
