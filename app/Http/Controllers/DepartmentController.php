6a932f8f2e6a2a3ca11301437f24b5ce8b6780d7
radwa-gamal30 <radwagamal482@gmail.com>
2024-09-16 12:25:39 +0300

    deparment

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -7,5 +7,45 @@ use Illuminate\Http\Request;
 
 class DepartmentController extends Controller
 {
-    //
+    public function index(){
+        $departments=department::get();
+        return response()->json(['departments'=>$departments]);
+    }
+   
+     public function store(Request $request){
+        $department= $request->validate([
+            'department_name'=>'required|string|max:255'
+        ]);
+   
+        department::create($request->all());
+        return response()->json([
+            'message' => 'department added successfully',
+            'recently added'=>$department],201
+            );
+    }
+    public function show(department $department){
+     
+        $department=department::find($department);
+        return response()->json(['department'=>$department]);
+    }
+
+     public function update(Request $request,department $department){
+        $request->validate([
+            'department_name'=>'required|string|max:255'
+        ]);
+   
+        $department->update($request->all());
+        return response()->json([
+            'message' => 'department updated successfully',
+            'recently updated'=>$department],201
+            );
+    }
+     public function destroy(department $department){
+        $department->delete();
+        return response()->json([
+            'message'=>'department deleted successfully',
+            'recently deleted'=>$department
+        ],201);
+    
+     }
 }
